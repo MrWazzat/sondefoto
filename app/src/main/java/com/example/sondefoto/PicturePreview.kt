1533df41b7700b9429d74d1a96d121c1e2c142b6
Raphael MARZAT <rmarzat@DESKTOP-ECMHC72.localdomain>
2022-10-25 08:25:09 +0200

    :bug: fix enregistrement de la photo

--- app/src/main/java/com/example/sondefoto/PicturePreview.kt
+++ app/src/main/java/com/example/sondefoto/PicturePreview.kt
@@ -1,20 +1,28 @@
 package com.example.sondefoto
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.media.MediaScannerConnection
+import android.opengl.ETC1.getWidth
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.telephony.TelephonyManager
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.sondefoto.communication.SondeforMailer
 import com.example.sondefoto.databinding.ActivityPicturePreviewBinding
 import java.io.File
 import java.io.FileOutputStream
+import java.lang.Exception
+import java.nio.file.Files
+import java.nio.file.Paths
 
 
 class PicturePreview : AppCompatActivity() {
@@ -32,24 +40,7 @@ class PicturePreview : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPicturePreviewBinding.inflate(layoutInflater)
 
-        // Create obj of TelephonyManager and ask for current telephone service
-        val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
         var phoneNumber = "?????????"
-//        if(ActivityCompat.checkSelfPermission(
-//                this,
-//                Manifest.permission.READ_SMS
-//            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-//                this,
-//                Manifest.permission.READ_PHONE_NUMBERS
-//            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-//                this,
-//                Manifest.permission.READ_PHONE_STATE
-//            ) != PackageManager.PERMISSION_GRANTED
-//        ) {
-//            phoneNumber = telephonyManager.line1Number
-//        }else{
-//            requestPermissions(arrayOf(Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_SMS, Manifest.permission.READ_PHONE_NUMBERS), permission);
-//        }
 
         setContentView(binding.root)
 
@@ -61,6 +52,7 @@ class PicturePreview : AppCompatActivity() {
         val bitmap = BitmapFactory.decodeFile(imagePath)
 
         binding.imageView.setImageBitmap(bitmap)
+
         binding.tvNumChantier.text = String.format("Chantier n° %s", sessionmanager.getString(NUM_CHANTIER_KEY))
 
         val loadingDialog = LoadingDialog(this)
@@ -69,7 +61,6 @@ class PicturePreview : AppCompatActivity() {
             loadingDialog.startLoadingAnimation()
             if (imagePath != null) {
                 persistImageToPicturesDirectory(bitmap)
-
                 SondeforMailer.Builder()
                     .firstName(sessionmanager.getString(FIRST_NAME_KEY).orEmpty())
                     .lastName(sessionmanager.getString(LAST_NAME_KEY).orEmpty())
@@ -79,6 +70,7 @@ class PicturePreview : AppCompatActivity() {
                     .attachmentPath(imagePath)
                     .build()
                     .sendMail(applicationContext)
+
             }
         }
 
@@ -87,18 +79,40 @@ class PicturePreview : AppCompatActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     fun persistImageToPicturesDirectory(bitmap: Bitmap){
-        val directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-        val newFile = File(directory, sondeforDirectoryName + System.currentTimeMillis()+ ".jpg")
+        // ERROR : java.io.filenotfoundexception /storage/emulated/0/
+        ActivityCompat.requestPermissions(this,  arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.MANAGE_EXTERNAL_STORAGE), 1)
+        try{
+            val directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).path + "/" + sondeforDirectoryName
+            val newFile = createFile(directory, System.currentTimeMillis().toString() + ".jpg")
 
-        val outputStream = FileOutputStream(newFile)
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
 
-        outputStream.flush();
-        outputStream.getFD().sync();
-        outputStream.close();
+            val outputStream = FileOutputStream(newFile)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            outputStream.flush();
+            outputStream.getFD().sync();
+            outputStream.close();
 
-        MediaScannerConnection.scanFile(this, arrayOf(newFile.getAbsolutePath()), null, null);
+            MediaScannerConnection.scanFile(this, arrayOf(newFile.getAbsolutePath()), null, null);
+        }catch (e: Exception){
+            Toast.makeText(this, "Erreur lors de l'enregistrement de la photo", Toast.LENGTH_SHORT).show()
+            e.printStackTrace()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun createFile(directoryPath: String, fileName: String): File{
+        Files.createDirectories(Paths.get(directoryPath))
+
+        val newFile = File(directoryPath, fileName)
+
+        if(!newFile.exists()){
+            newFile.createNewFile()
+        }
+
+        return newFile
     }
 
     fun openSaisieChantier(){
